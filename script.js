1d1c13524c9d59f81249771935c31840e620c91a
Lucas da Silva Oliveira <lucas-silva-98@hotmail.com>
2023-05-03 23:15:55 -0300

    Adicionei imagem de load para o sorteio não ser instantâneo

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const input_times = document.getElementById('times');
 const nomes_adicionados = document.getElementById('nomes-adicionados');
 const times_adicionados = document.getElementById('times-adicionados');
 const sorteio_feito = document.getElementById('sorteio-feito');
+const img = document.getElementById('img');
+const btn_sortear = document.getElementById('btn-sortear');
 
 let nomeArray = [];
 let timeArray = [];
@@ -27,20 +29,25 @@ function adicionar_time() {
 
 function sortear() {
 
-    if(nomeArray.length == 0) {
+    if (nomeArray.length == 0) {
         window.alert('Não há nomes para sortear.');
         return;
-    } 
-    
-    let nomeAleatorio = nomeArray[Math.floor(Math.random() * nomeArray.length)];
-    let timeAleatorio = timeArray[Math.floor(Math.random() * timeArray.length)];
-    let p = document.createElement('p');
-    p.innerHTML = `${nomeAleatorio} - ${timeAleatorio}`;
-    sorteio_feito.appendChild(p);
-    let indexNomeRemove = nomeArray.indexOf(nomeAleatorio);
-    nomeArray.splice(indexNomeRemove,1);
-    let indexTimeRemove = timeArray.indexOf(timeAleatorio);
-    timeArray.splice(indexTimeRemove,1);
+    }
 
-      
+    img.classList.add('adicionar-rodar');
+    btn_sortear.style.display = 'none';
+
+    setTimeout(() => {
+        btn_sortear.style.display = 'block';
+        img.classList.remove('adicionar-rodar');
+        let nomeAleatorio = nomeArray[Math.floor(Math.random() * nomeArray.length)];
+        let timeAleatorio = timeArray[Math.floor(Math.random() * timeArray.length)];
+        let p = document.createElement('p');
+        p.innerHTML = `${nomeAleatorio} - ${timeAleatorio}`;
+        sorteio_feito.appendChild(p);
+        let indexNomeRemove = nomeArray.indexOf(nomeAleatorio);
+        nomeArray.splice(indexNomeRemove, 1);
+        let indexTimeRemove = timeArray.indexOf(timeAleatorio);
+        timeArray.splice(indexTimeRemove, 1);
+    }, 2000);
 }
